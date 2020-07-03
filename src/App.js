6fa98aa7af2d0cb85dfdeb5bffab53628c215f10
Matt Wilkins <Matt@MBP-2.local>
2020-07-03 09:48:29 +0100

    Added a paragraph

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Matt/'s first React App
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
