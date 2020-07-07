c65fb05fd7debb95b2e93434ba996cbde941c04f
Matt Wilkins <Matt@MBP-2.local>
2020-07-07 09:44:23 +0100

    typos

--- src/competition.js
+++ src/competition.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 const Competition = props => (
-  <header className="competition">
-    <h1>
-	  <span>{props.title}</span>
-    </h1>
-    <h3 className="closing-date">
-      <span>{props.closing-date}</span>
-    </h3>
-  </header>
+	let props = {
+		title:"-",
+		closingdate:"-"
+	}
+	<competition className="competition">
+		<h1>
+			<span>{props.title}</span>
+		</h1>
+		<h3 className="closing-date">
+			<span>{props.closingdate}</span>
+		</h3>
+	</competition>
 );
 
 export default Competition;
